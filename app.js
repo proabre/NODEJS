aa8389010ad5a01e559029f32c0d4ad3548f80ca
proabre <proabresh@gmail.com>
2026-09-11 22:30:53 +0300

    export and importing more than one function at once

--- app.js
+++ app.js
@@ -7,4 +7,18 @@ const Multiplication = require("./export-multipl.js"); //importing exported modu
 
 const product1 = Multiplication(30, 3); //passing values after importing modules
 
+const product2 = Multiplication(10, 2);
+
 console.log(product1);
+console.log(product2);
+
+//advance importing with array distructuring
+
+const calc = require("./calc.js");
+
+const { add, subtract } = calc; //array distructuring
+
+console.log("advanced importing");
+
+console.log(add(3, 5));
+console.log(subtract(10, 9));
---
+++ calc.js
@@ -0,0 +1,12 @@
+const add = (x, y) => {
+  return x + y;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+module.exports = {
+  add,
+  subtract,
+};
